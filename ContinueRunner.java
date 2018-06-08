f3c35e565c241f901614ee2ab182f6b360cd4975
Nidhi Bansal <39649398+NidhiBans@users.noreply.github.com>
2018-06-08 19:22:28 +0530

    Add files via upload

---
+++ ContinueRunner.java
@@ -0,0 +1,16 @@
+package com.Loops;
+
+public class ContinueRunner {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println("even numbers are:" + i);
+			} else {
+				continue;
+			}
+		}
+
+	}
+
+}
