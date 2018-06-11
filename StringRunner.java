8454198603d2770725765c1c6e8d7467f1c18fe0
Nidhi Bansal <39649398+NidhiBans@users.noreply.github.com>
2018-06-11 14:34:13 +0530

    Add files via upload

---
+++ StringRunner.java
@@ -0,0 +1,18 @@
+package com.strings;
+
+public class StringRunner {
+
+	public static void main(String[] args) {
+		String str = "This is a string";
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i));
+		}
+
+			System.out.println(str.contains("is"));
+		System.out.println(1 + 2 + "3");
+		System.out.println(str.replace("T", "n"));
+		}
+
+	}
+
+
---
+++ WrapperClass.java
@@ -0,0 +1,17 @@
+package com.strings;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		Integer i = Integer.valueOf(7);
+		Integer j = Integer.valueOf(8);
+		System.out.println(i == j);
+		int max = Integer.MAX_VALUE;
+		System.out.println(max);
+
+
+
+
+	}
+
+}
