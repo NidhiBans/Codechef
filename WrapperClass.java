978bf9284940fa83e767bd8b7f5f594c682efa75
Nidhi Bansal <39649398+NidhiBans@users.noreply.github.com>
2018-06-11 14:36:52 +0530

    Update WrapperClass.java

--- WrapperClass.java
+++ WrapperClass.java
@@ -3,10 +3,10 @@ package com.strings;
 public class WrapperClass {
 
 	public static void main(String[] args) {
-		Integer i = Integer.valueOf(7);
+		Integer i = Integer.valueOf(7);//way to create a wrapper class
 		Integer j = Integer.valueOf(8);
-		System.out.println(i == j);
-		int max = Integer.MAX_VALUE;
+		System.out.println(i == j);//comparing reference variables
+		int max = Integer.MAX_VALUE;//returning max value integer can hold
 		System.out.println(max);
 
 
